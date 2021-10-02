503b1af20ef70b5f86520f0c3461d8db692ab336
Faustabu <90148059+Faustabu@users.noreply.github.com>
2021-10-02 17:55:06 +0300

    Update nuskaitymas.cpp

--- nuskaitymas.cpp
+++ nuskaitymas.cpp
@@ -33,8 +33,27 @@ int main()
     char temp;
     vector<studentas> Eil;
     skaitymas(Eil, &pazymiu_sk);
-    spausdinimas(Eil, pazymiu_sk);
+    //spausdinimas(Eil, pazymiu_sk);
     //system("pause");
+    cout << setw(20) << std::left << "Vardas"
+        << setw(20) << std::left << "Pavarde"
+        << setw(18) << std::left << "Galutinis(vid.)" << "\n"
+        << "--------------------------------------------------------------------------\n";
+    for (int i = 0; i < Eil.size(); i++)
+    {
+         for (int j = i + 1; j < Eil.size(); j++) {
+            if (Eil[i].vardas > Eil[j].vardas) {
+                std::swap(Eil[i].vardas, Eil[j].vardas);
+                std::swap(Eil[i].pavarde, Eil[j].pavarde);
+                std::swap(Eil[i].galvid, Eil[j].galvid);
+                }
+         }
+        cout << setw(20) << std::left<< Eil[i].vardas
+            << setw(20) << std::left << Eil[i].pavarde
+            << setw(18) << std::left <<fixed << setprecision(2)<< Eil[i].galvid << endl;
+
+    }
+    cout << "\n\n";
     return 0;
 }
 
@@ -51,7 +70,7 @@ unsigned int countWordsInString(string const& str)
 void skaitymas(vector<studentas>& Eil, int* pazymiu_sk)
 {
     int student_counter = 0;
-    int temp;
+    float temp, s ;
     std::ifstream fileRead;
     string buff;
     fileRead.open("kursiokai.txt");
@@ -66,15 +85,25 @@ void skaitymas(vector<studentas>& Eil, int* pazymiu_sk)
             fileRead >> Eil.at(student_counter).vardas;
             if (fileRead.eof()) { Eil.pop_back(); break; }
             fileRead >> Eil.at(student_counter).pavarde;
+            s=0;
             for (int i = 0; i < *pazymiu_sk; i++)
+          
             {
                 fileRead >> temp;
                 Eil.at(student_counter).nd.push_back(temp);
+                s+=temp;
+                
             }
+           // std::sort(Eil.at(student_counter).nd, );
+
             fileRead >> Eil.at(student_counter).egz;
 
-            Eil.at(student_counter).galvid = Eil.at(student_counter).galvid / *pazymiu_sk;
+            Eil.at(student_counter).galvid = s / *pazymiu_sk;
+            
             Eil.at(student_counter).galvid = Eil.at(student_counter).galvid * 0.4 + 0.6 * Eil.at(student_counter).egz;
+
+           // Eil.at(student_counter).galmed = 
+
             student_counter++;
         }
     }
@@ -85,7 +114,7 @@ void skaitymas(vector<studentas>& Eil, int* pazymiu_sk)
 
 
 
-void spausdinimas(vector<studentas> Eil, int pazymiu_sk)
+/*void spausdinimas(vector<studentas> Eil, int pazymiu_sk)
 {
     std::ofstream output;
     output.open("rezultatai.txt");
@@ -105,4 +134,4 @@ void spausdinimas(vector<studentas> Eil, int pazymiu_sk)
 
     }
     output << "\n\n";
-}
+}*/
