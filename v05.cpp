8783809ba4d54859c9c35dacc20379933074f25c
Faustabu <90148059+Faustabu@users.noreply.github.com>
2021-11-17 18:32:33 +0200

    Update v05.cpp

--- v05.cpp
+++ v05.cpp
@@ -1,5 +1,141 @@
 #include "studentas.h"
+using std::cout;
+using std::cin;
+using std::endl;
 
+
+int main()
+{
+    int paz;
+    cout << "iveskite pazymiu kieki: ";
+    cin >> paz;
+    float balas = 5.00;
+    auto_skaiciai(paz);
+    vector<int> skaiciai;
+
+    int kiek = generavimas(skaiciai);
+    cout << kiek << endl;
+    list <studentas> duom;
+    vector<studentas> duomvec;
+    list<studentas> galvociai;
+    list<studentas> nuskriaustukai;
+    vector<studentas> galvociaivec;
+    vector<studentas> nuskriaustukaivec;
+    int galvoc = 0;
+    int nuskr = 0;
+    string pavad1, pavad2; 
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    cout << std::fixed;
+
+    failo_skaitymas(duom, kiek);
+
+    start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& laik : duom) {
+        if (laik.gal < balas) {
+            nuskriaustukai.push_back(laik);
+        }
+        else {
+            galvociai.push_back(laik);
+        }
+    }
+
+    end = std::chrono::high_resolution_clock::now();
+    diff = end - start;
+    cout << "Studentu rusiavimas i dvi grupes su list uztruko:  " << diff.count() << endl;
+
+    start = std::chrono::high_resolution_clock::now();
+    pavad1 = "nuskriaustukai_" + std::to_string(kiek) + ".txt";
+    std::ofstream nuskr_failas(pavad1);
+
+    for (int l = 0; l < kiek; l++)
+    {
+        if (get(duom, l).gal < balas) {
+
+            nuskr_failas << get(duom, l).vardas << std::setw(20) << get(duom, l).pavarde << std::setw(20) << get(duom, l).gal << endl;
+        }
+    }
+
+
+    end = std::chrono::high_resolution_clock::now();
+    diff = end - start;
+    //cout << "Surūšiuotų studentų išvedimas į failą - nuskriaustukai:  " << diff.count() << endl;
+
+
+
+    start = std::chrono::high_resolution_clock::now();
+
+    pavad2 = "galvociai_" + std::to_string(kiek) + ".txt";
+    std::ofstream galvoc_failas(pavad2);
+
+    for (int k = 0; k < kiek; k++)
+    {
+        if (get(duom, k).gal >= balas) {
+
+            galvoc_failas << get(duom, k).vardas << std::setw(20) << get(duom, k).pavarde << std::setw(20) << get(duom, k).gal << endl;
+        }
+    }
+    duom.clear();
+    galvociai.clear();
+    nuskriaustukai.clear();
+
+    end = std::chrono::high_resolution_clock::now();
+    diff = end - start;
+    //cout << "Surūšiuotų studentų išvedimas į failą - galvočiai:  " << diff.count() << endl;
+
+    failo_skaitymasvec(duomvec, kiek);
+
+    start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& laik : duomvec) {
+        if (laik.gal < balas) {
+            nuskriaustukaivec.push_back(laik);
+        }
+        else {
+            galvociaivec.push_back(laik);
+        }
+    }
+    duomvec.clear();
+    galvociaivec.clear();
+    nuskriaustukaivec.clear();
+    duomvec.shrink_to_fit();
+    galvociaivec.shrink_to_fit(); 
+    nuskriaustukaivec.shrink_to_fit();
+
+    end = std::chrono::high_resolution_clock::now();
+    diff = end - start;
+    cout << "Studentu rusiavimas i dvi grupes su vector uztruko:  " << diff.count() << endl;
+
+}
+
+
+void failo_skaitymasvec(vector<studentas>& kint, int kiek) {
+    int stud = 0;
+    std::ifstream skaitymas;
+    string buff;
+    string pavadinimas = "studentai_" + std::to_string(kiek) + ".txt";
+
+    auto start = std::chrono::high_resolution_clock::now();
+    skaitymas.open(pavadinimas);
+
+    if (skaitymas.is_open()) {
+        getline(skaitymas >> std::ws, buff);
+        while (stud < kiek)
+        {
+            kint.resize(kint.size() + 1);
+
+            skaitymas >> kint.at(stud).vardas;
+            skaitymas >> kint.at(stud).pavarde;
+            skaitymas >> kint.at(stud).gal;
+            stud += 1;
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    cout << "nuskaitymas su vectoriumi uztruko: " << diff.count() << endl;
+}
 studentas get(list<studentas> lst, int d) {
     list<studentas>::iterator itr = lst.begin();
     for (int i = 0; i < d; i++) ++itr;
@@ -7,10 +143,11 @@ studentas get(list<studentas> lst, int d) {
 }
 
 int random_generatorius()
-{
-    // srand(time(NULL));
-    int r = rand() % 10 + 1;
-    return r;
+{       
+     using hrClock = std::chrono::high_resolution_clock;
+     std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+     std::uniform_int_distribution<int> dist(0, 10);
+     return dist(mt);
 }
 
 vector<int> auto_skaiciai(int pazymiu_kiekis)
@@ -85,85 +222,7 @@ void failo_skaitymas(list<studentas>& kint, int kiek) {
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> diff = end - start;
-        cout << "nuskaitymas uztruko: " << diff.count() << endl;
+        cout << "nuskaitymas su listu uztruko: " << diff.count() << endl;
     }
 
-}
-
-
-int main()
-{
-    int paz;
-    cout << "iveskite pazymiu kieki: ";
-    cin >> paz;
-    float balas = 5.00;
-    auto_skaiciai(paz);
-    vector<int> skaiciai;
-
-    int kiek = generavimas(skaiciai);
-    cout << kiek << endl;
-    list <studentas> duom;
-    failo_skaitymas(duom, kiek);
-    list<studentas> galvociai;
-    list<studentas> nuskriaustukai;
-    int galvoc = 0;
-    int nuskr = 0;
-    string pavad1, pavad2;
-
-    auto start1 = std::chrono::high_resolution_clock::now();
-
-
-   for (const auto& laik : duom) {
-        if (laik.gal < balas) {
-            nuskriaustukai.push_back(laik);
-        }
-        else {
-            galvociai.push_back(laik);
-        }
-   }
-
-    auto end1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff1 = end1 - start1;
-    cout << "Studentu rusiavimas i dvi grupes uztruko:  " << diff1.count() << endl;
-
-    auto start2 = std::chrono::high_resolution_clock::now();
-
-
-    pavad1 = "nuskriaustukai_" + std::to_string(kiek) + ".txt";
-    std::ofstream nuskr_failas(pavad1);
-
-    for (int l = 0; l < kiek; l++)
-    {
-        if (get(duom, l).gal < balas) {
-
-            nuskr_failas << get(duom, l).vardas << std::setw(20) << get(duom, l).pavarde << std::setw(20) << get(duom, l).gal << endl;
-        }
-    }
-    
-
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff2 = end2 - start2;
-    //cout << "Surūšiuotų studentų išvedimas į failą - nuskriaustukai:  " << diff2.count() << endl;
-
-
-
-    auto start3 = std::chrono::high_resolution_clock::now();
-
-    pavad2 = "galvociai_" + std::to_string(kiek) + ".txt";
-    std::ofstream galvoc_failas(pavad2);
-
-    for (int k = 0; k < kiek; k++)
-    {
-        if (get(duom, k).gal >= balas) {
-
-            galvoc_failas << get(duom, k).vardas << std::setw(20) << get(duom, k).pavarde << std::setw(20) << get(duom, k).gal << endl;
-        }
-    }
-    duom.clear();
-    galvociai.clear();
-    nuskriaustukai.clear();
-    
-    auto end3 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff3 = end3 - start3;
-    //cout << "Surūšiuotų studentų išvedimas į failą - galvočiai:  " << diff3.count() << endl;
 }
